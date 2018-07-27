579261fc275ea4f8a0d68e7dde01404d4467be79
tsubasa ooishi <pop331240283@icloud.com>
2018-07-27 15:53:13 +0900

    Finish 5-9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
 			redirect_to user_path(@user.id)
 			flash[:notice]="User was successfully updated."
 		else
-			redirect_to action: 'show'
+			render 'edit'
 		end
 	end
 
